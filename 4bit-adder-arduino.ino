92c5163d45072c58d57a46081e3d3b1388374178
paul-madut <paul.madut@mths.ca>
2023-06-07 22:41:42 -0400

    Update 4bit-adder-arduino.ino

--- 4bit-adder-arduino.ino
+++ 4bit-adder-arduino.ino
@@ -21,10 +21,10 @@ void setup()
     int b[] = {b1, b2, b3, b4};
 
     // input pins for the
-    int a_down = A0;
-    int a_up = A1;
-    int b_down = A2;
-    int b_up = A3;
+    int a_down = A1;
+    int a_up = A0;
+    int b_down = A3;
+    int b_up = A2;
 
     Serial.begin(9600);
 
@@ -38,16 +38,15 @@ void setup()
     pinMode(b3, OUTPUT);
     pinMode(b4, OUTPUT);
 
-    pinMode(a_up, OUTPUT);
-    pinMode(a_down, OUTPUT);
-    pinMode(b_up, OUTPUT);
-    pinMode(b_down, OUTPUT);
-
-    countInput(b, bits(4));
+    pinMode(a_up, INPUT);
+    pinMode(a_down, INPUT);
+    pinMode(b_up, INPUT);
+    pinMode(b_down, INPUT);
 }
 
 void loop()
 {
+    testButtons(A0, A1, A2, A3);
 }
 void testLights()
 {
@@ -98,4 +97,30 @@ void countInput(int array[], int bits)
 int bits(int numberOfBits)
 {
     return pow(2, numberOfBits);
+}
+
+void testButtons(int a_up, int a_down, int b_up, int b_down)
+{
+    int buttonState1 = analogRead(a_up);
+    int buttonState2 = analogRead(a_down);
+    int buttonState3 = analogRead(b_up);
+    int buttonState4 = analogRead(b_down);
+
+    // Check if the button is pressed
+    if (buttonState1 == 0)
+    {
+        digitalWrite(3, HIGH);
+    }
+    if (buttonState2 == 0)
+    {
+        digitalWrite(3, LOW);
+    }
+    if (buttonState3 == HIGH)
+    {
+        Serial.println("Button pressed!");
+    }
+    if (buttonState4 == HIGH)
+    {
+        Serial.println("Button pressed!");
+    }
 }
